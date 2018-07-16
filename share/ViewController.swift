95df598e93257e3616ad495e06afe67925eb82ce
rexzx <guweixu@gmail.com>
2018-07-16 17:44:46 +0800

    no message

--- share/ViewController.swift
+++ share/ViewController.swift
@@ -9,17 +9,90 @@
 import UIKit
 
 class ViewController: UIViewController {
+    @IBOutlet weak var getVideoURLLabel : UILabel!
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    @IBAction func shareFbWeb(_ sender: Any) {
     }
-
-
+    @IBAction func shareFbPhotos(_ sender: Any) {
+    }
+    
+    @IBAction func shareFbVid(_ sender: Any) {
+    }
+    
+    @IBAction func shareFbMedia(_ sender: Any) {
+    }
+    
+    @IBAction func shareTwWeb(_ sender: Any) {
+    }
+    
+    @IBAction func shareInsApp(_ sender: Any) {
+    }
+  
+    @IBAction func shareInsSys(_ sender: Any) {
+    }
+   
+    @IBAction func shareInsVid(_ sender: Any) {
+    }
+    @IBAction func shareTextWx(_ sender: Any) {
+    }
+    @IBAction func sharePhWx(_ sender: Any) {
+    }
+    @IBAction func shareWebWx(_ sender: Any) {
+    }
+    @IBAction func shareVideoWx(_ sender: Any) {
+    }
+    @IBAction func shareMsicWx(_ sender: Any) {
+    }
+    @IBAction func shareMiniProgrWx(_ sender: Any) {
+    }
+    @IBAction func shareFileWx(_ sender: Any) {
+    }
+    
+    @IBAction func shareTextWb(_ sender: Any) {
+    }
+    @IBAction func sharePhWb(_ sender: Any) {
+    }
+    @IBAction func shareVidWb(_ sender: Any) {
+    }
+    @IBAction func shareWebWb(_ sender: Any) {
+    }
+    @IBAction func shareTextQq(_ sender: Any) {
+    }
+    @IBAction func shareImgQq(_ sender: Any) {
+    }
+    @IBAction func shareWebQq(_ sender: Any) {
+    }
+    @IBAction func shareVidQq(_ sender: Any) {
+    }
+    @IBAction func shareAudQq(_ sender: Any) {
+    }
+    @IBAction func shareTextQz(_ sender: Any) {
+    }
+    @IBAction func shareImgsQz(_ sender: Any) {
+    }
+    @IBAction func shareVidQz(_ sender: Any) {
+    }
+    @IBAction func shareTextWsa(_ sender: Any) {
+    }
+    @IBAction func shareImgWsa(_ sender: Any) {
+    }
+    @IBAction func shareUrlGplus(_ sender: Any) {
+    }
+    @IBAction func shareTextTm(_ sender: Any) {
+    }
+    @IBAction func shareImgTm(_ sender: Any) {
+    }
+    @IBAction func getVideoURL(_ sender: Any) {
+    }
+    
+    
+}
+extension ViewController {
+    
 }
 
