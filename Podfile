53a0303098cdc2367836e496545d4e97a97ac712
rexzx <guweixu@gmail.com>
2018-07-17 10:51:26 +0800

    no message

---
+++ Podfile
@@ -0,0 +1,11 @@
+platform :ios, '9.0'
+
+target 'share' do
+    use_frameworks!
+   
+end
+target 'sharekit' do
+    use_frameworks!
+    pod 'Flurry-iOS-SDK/TumblrAPI'
+    
+end
---
+++ sharekit/sharekit.h
@@ -0,0 +1,19 @@
+//
+//  sharekit.h
+//  sharekit
+//
+//  Created by valenti on 2018/7/17.
+//  Copyright © 2018 rex. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for sharekit.
+FOUNDATION_EXPORT double sharekitVersionNumber;
+
+//! Project version string for sharekit.
+FOUNDATION_EXPORT const unsigned char sharekitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <sharekit/PublicHeader.h>
+
+
